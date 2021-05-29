1b140f14ec862956751338fceefdad3dbdd56e50
Leah Lundqvist <leah@pigeon.sh>
2021-05-29 23:04:35 +0200

    api: client part 2 - APIClient

--- src/api/client.ts
+++ src/api/client.ts
@@ -1,3 +1,5 @@
+import APIRequest from "./request"
+
 export enum Method {
   get = 'GET',
   put = 'PUT',
@@ -5,3 +7,64 @@ export enum Method {
   delete = 'DELETE',
   patch = 'PATCH'
 }
+
+class APIClient {
+  authorization: string
+  globalLimit: number = 50
+  globalRemaining: number = this.globalLimit
+  globalReset: number|undefined
+  globalDelay: Promise<void>|undefined
+
+  constructor (token: string) {
+    this.authorization = `Bot ${token}`
+  }
+
+  async enqueue (request: APIRequest): Promise<object | Buffer | undefined> {
+    // TOOD: implement buckets
+    return
+  }
+
+  async get (path: string): Promise<object | Buffer | undefined> {
+    return this.enqueue(new APIRequest({
+      path,
+      method: Method.get,
+      headers: { authorization: this.authorization }
+    }))
+  }
+
+  async put (path: string): Promise<object | Buffer | undefined> {
+    return this.enqueue(new APIRequest({
+      path,
+      method: Method.put,
+      headers: { authorization: this.authorization }
+    }))
+  }
+
+  async post (path: string, body: any): Promise<object | Buffer | undefined> {
+    return this.enqueue(new APIRequest({
+      path,
+      method: Method.post,
+      headers: { authorization: this.authorization },
+      body
+    }))
+  }
+
+  async patch (path: string, body: any): Promise<object | Buffer | undefined> {
+    return this.enqueue(new APIRequest({
+      path,
+      method: Method.patch,
+      headers: { authorization: this.authorization },
+      body
+    }))
+  }
+
+  async delete (path: string): Promise<object | Buffer | undefined> {
+    return this.enqueue(new APIRequest({
+      path,
+      method: Method.delete,
+      headers: { authorization: this.authorization }
+    }))
+  }
+}
+
+export default APIClient
