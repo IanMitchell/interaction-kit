79b09be7c15f862d4dd947388b7c6963e1e234ed
Zelda_Fan <78901316+ZeldaFan0225@users.noreply.github.com>
2022-01-16 19:03:41 +0100

    Updated Types and Links (#101)

--- packages/interaction-kit/src/definitions/messages.ts
+++ packages/interaction-kit/src/definitions/messages.ts
@@ -14,8 +14,14 @@ import { Reaction } from "./reactions";
 import { StickerItem } from "./stickers";
 import { MessageInteraction } from "./application-commands";
 
-// TODO: Document
-export enum PermissionFlags {
+/** @link https://discord.com/developers/docs/resources/channel#message-object-message-flags */
+export enum MessageFlags {
+	CROSSPOSTED = 1 << 0,
+	IS_CROSSPOSTED = 1 << 1,
+	SUPRESS_EMBEDS = 1 << 2,
+	SOURCE_MESSAGE_DELETED = 1 << 3,
+	URGENT = 1 << 4,
+	HAS_THREAD = 1 << 5,
 	EPHEMERAL = 1 << 6,
 	LOADING = 1 << 7,
 }
@@ -46,7 +52,7 @@ export type Message = {
 	application?: unknown; // TODO: type this
 	application_id?: Snowflake;
 	message_reference?: MessageReference;
-	flags?: number;
+	flags?: MessageFlags;
 	referenced_message?: Message | null;
 	interaction?: MessageInteraction;
 	thread?: Channel;
