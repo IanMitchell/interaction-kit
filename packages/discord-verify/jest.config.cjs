d4b4210e06b697141512af0832ac508666cb9c52
Ian Mitchell <ian.mitchell@hey.com>
2022-06-03 20:47:10 -0700

    remove old files

--- packages/discord-verify/jest.config.cjs
+++
@@ -1,8 +0,0 @@
-// @ts-check
-/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-module.exports = {
-	preset: "ts-jest",
-	testEnvironment: "node",
-	clearMocks: true,
-	resetMocks: true,
-};
