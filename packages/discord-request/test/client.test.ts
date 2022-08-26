adba9b08b3b1ff1332656d0797754795bc0ed8be
Ian Mitchell <ian.mitchell@hey.com>
2022-08-26 16:38:12 -0700

    sketch out method test

--- packages/discord-request/test/client.test.ts
+++ packages/discord-request/test/client.test.ts
@@ -1,4 +1,5 @@
-import { describe, test } from "vitest";
+import { afterEach, beforeEach, describe, expect, test, vi } from "vitest";
+import Client from "../src/client.js";
 
 describe("Configuration", () => {
 	test.todo("setToken");
@@ -19,7 +20,40 @@ describe("Configuration", () => {
 });
 
 describe("HTTP Methods", () => {
-	test.todo("Handles GET requests");
+	let spy;
+
+	beforeEach(() => {
+		spy = vi.fn();
+
+		vi.mock("../src/manager", () => {
+			const klass = class Manager {
+				queue: unknown;
+
+				constructor() {
+					this.queue = spy;
+				}
+			};
+
+			return { Manager: klass };
+		});
+	});
+
+	afterEach(() => {
+		vi.clearAllMocks();
+	});
+
+	test("Handles GET requests", async () => {
+		const client = new Client();
+		await client.get("/get", {
+			query: new URLSearchParams([["key", "value"]]),
+		});
+
+		expect(spy).toHaveBeenCalledWith(
+			"/get",
+			{ query: new URLSearchParams([["key", "value"]]) },
+			"GET"
+		);
+	});
 
 	test.todo("Handles POST requests");
 
