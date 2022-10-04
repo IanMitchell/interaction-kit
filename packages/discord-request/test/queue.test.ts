82b1a1ce47534c1e58f834d42abeebe264954b97
Leah Lundqvist <leah@pigeon.sh>
2022-10-04 20:53:22 +0200

    test/queue(recovery from error in queue)

--- packages/discord-request/test/queue.test.ts
+++ packages/discord-request/test/queue.test.ts
@@ -1,4 +1,8 @@
-import { describe, test } from "vitest";
+import { DiscordError } from "discord-error";
+import { Routes } from "discord.js";
+import { describe, expect, test, vi } from "vitest";
+import { Manager } from "../src/manager";
+import { RequestMethod } from "../src/types";
 
 describe("Rate Limits", () => {
 	test.todo("Throttles Global Rate Limited requests");
@@ -35,7 +39,58 @@ describe("Requests", () => {
 describe("Error Handling", () => {
 	test.todo("Retries Timeouts");
 
-	test.todo("Request errors do not break Queue");
+	test("Request errors do not break Queue", async () => {
+		// mock global fetch to throw an unauthorized error on the first request
+		let requestCounts = 0;
+		vi.stubGlobal(
+			"fetch",
+			vi.fn(() => {
+				// throw the first request
+				if (requestCounts++ === 0) {
+					return new Response(
+						JSON.stringify({ message: "401: Unauthorized", code: 0 }),
+						{
+							status: 401,
+							statusText: "Unauthorized",
+							headers: {
+								"Content-Type": "application/json",
+							},
+						}
+					);
+				}
+				return new Response(
+					JSON.stringify({ url: "wss://gateway.discord.gg" }),
+					{
+						status: 200,
+						statusText: "OK",
+						headers: {
+							"Content-Type": "application/json",
+						},
+					}
+				);
+			})
+		);
+
+		const manager = new Manager({});
+
+		manager.setToken("token");
+		try {
+			await manager.queue({
+				method: RequestMethod.Get,
+				path: Routes.channel("839006448057974826"),
+			});
+		} catch (error) {
+			expect(error).toBeInstanceOf(DiscordError);
+		}
+
+		expect(
+			await manager.queue({
+				method: RequestMethod.Get,
+				path: Routes.gateway(),
+				auth: false,
+			})
+		).toHaveProperty("url");
+	});
 
 	test.todo("Handles Server Errors");
 
