08ceeb949cbf1fb57361eca87d67192ec3067873
Ian Mitchell <ian.mitchell@hey.com>
2022-10-18 16:36:56 -0700

    add docs

--- packages/discord-error/src/discord-error.ts
+++ packages/discord-error/src/discord-error.ts
@@ -35,6 +35,11 @@ export function isDiscordError(error: ErrorBody): error is DiscordErrorBody {
 	return "code" in error;
 }
 
+/**
+ * Determines whether the value is a group of errors
+ * @param error The record or piece of the record returned by the Discord API to check
+ * @returns whether the value contains a group of errors
+ */
 export function isErrorGroup(error: APIError): error is ErrorGroup {
 	if (error === null || typeof error !== "object") {
 		return false;
@@ -43,6 +48,11 @@ export function isErrorGroup(error: APIError): error is ErrorGroup {
 	return "_errors" in error;
 }
 
+/**
+ * Determines whether the value is an error value
+ * @param error The record or piece of the record returned by the Discord API to check
+ * @returns whether the value is an error value
+ */
 export function isErrorField(error: APIError): error is ErrorField {
 	if (error === null || typeof error !== "object") {
 		return false;
@@ -51,6 +61,11 @@ export function isErrorField(error: APIError): error is ErrorField {
 	return "message" in error;
 }
 
+/**
+ * Parses the errors returned by the Discord API into an error message string
+ * @param error The record returned by the Discord API
+ * @returns The parsed error message
+ */
 export function getMessage(error: ErrorBody) {
 	if (isDiscordError(error)) {
 		const stack = [];
@@ -122,11 +137,34 @@ function getNextKey(key: string | null, label: string) {
 }
 
 export class DiscordError extends Error {
+	/**
+	 * The error code returned by the Discord API
+	 */
 	code: string | number;
+
+	/**
+	 * The request that caused the error
+	 */
 	request: Request;
+
+	/**
+	 * The error response
+	 */
 	response: Response;
+
+	/**
+	 * The raw JSON body of the error response. If you are looking at the data
+	 * programmatically, use this field in conjunction with the `code` field.
+	 */
 	raw: ErrorBody;
 
+	/**
+	 * Creates a new DiscordError
+	 * @param request The request that caused the error
+	 * @param response The error response
+	 * @param code The error code
+	 * @param raw The raw JSON body of the error response
+	 */
 	constructor(
 		request: Request,
 		response: Response,
@@ -140,6 +178,9 @@ export class DiscordError extends Error {
 		this.raw = raw;
 	}
 
+	/**
+	 * The Discord error name and error code
+	 */
 	get name() {
 		return `${DiscordError.name}[${this.code}]`;
 	}
