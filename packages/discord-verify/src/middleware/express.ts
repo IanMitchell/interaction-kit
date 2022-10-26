10303f15e5acbedcaabe995c034b3167141a501e
Stuart Allan Rutherford <stalruth@gmail.com>
2022-10-26 16:09:00 +1100

    Make checks pass

--- packages/discord-verify/src/middleware/express.ts
+++ packages/discord-verify/src/middleware/express.ts
@@ -1,10 +1,8 @@
 import crypto from "node:crypto";
-import {
-  verify,
-  PlatformAlgorithm
-} from "../node.ts";
-import type { SubtleCryptoKeyAlgorithm } from "../types/index.js";
-import type { NextFunction, Request, Response } from "@types/express";
+import type { APIInteraction } from "discord-api-types/v10";
+import type { NextFunction, Request, Response } from "express";
+import { verify, PlatformAlgorithm } from "../node.js";
+import type { SubtleCryptoImportKeyAlgorithm } from "../types/index.js";
 
 /**
  * Returns middleware for Express.js that validates a request from Discord.
@@ -15,46 +13,44 @@ import type { NextFunction, Request, Response } from "@types/express";
  * functionally works the same as a body parser middleware such as
  * `express.json`.
  *
- * @param publicKey The application's public key
- * @param algorithm The name of the crypto algorithm to use
+ * @param publicKey - The application's public key
+ * @param algorithm - The name of the crypto algorithm to use
  * @returns An express.js middleware function that verifies a request from Discord.
  */
 export default function getMiddleware(
 	publicKey: string,
 	algorithm: SubtleCryptoImportKeyAlgorithm | string = PlatformAlgorithm.NewNode
 ) {
-	if(!publicKey) {
+	if (!publicKey) {
 		throw new Error("You must specify a Public Key.");
 	}
 
-	return async function(
-		req: Request,
-		res: Resposne,
-		next: NextFunction
-	) {
-		const timestamp = req.header("X-Signature-Timestamp") || "";
-		const signature = req.header("X-Signature-Ed25519") || "";
+	return async function (req: Request, res: Response, next: NextFunction) {
+		const timestamp = req.header("X-Signature-Timestamp") ?? "";
+		const signature = req.header("X-Signature-Ed25519") ?? "";
 
-		const chunks = [];
-		req.on('data', chunk => {
+		const chunks: Buffer[] = [];
+		req.on("data", (chunk: Buffer) => {
 			chunks.push(chunk);
 		});
-		req.on('end', async () => {
-			const body = Buffer.concat(chunks);
-			if (!await verify(
-				body,
-				signature,
-				timestamp,
-				publicKey,
-				crypto.webcrypto.subtle,
-				algorithm ?? PlatformAlgorithm.OldNode)
+		req.on("end", async () => {
+			const body = Buffer.concat(chunks).toString("utf-8");
+			if (
+				!(await verify(
+					body,
+					signature,
+					timestamp,
+					publicKey,
+					crypto.webcrypto.subtle,
+					algorithm ?? PlatformAlgorithm.OldNode
+				))
 			) {
-				res.status(401).send('[discord-verify] Invalid signature');
+				res.status(401).send("[discord-verify] Invalid signature");
 				return;
 			}
 
-			req.body = JSON.parse(body.toString('utf-8')) || {};
+			req.body = JSON.parse(body) as APIInteraction;
 			next();
 		});
-	}
+	};
 }
