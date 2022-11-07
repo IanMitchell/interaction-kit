ff5ee98f38c128d67af301c5c84ab3c8b2feaef3
Ian Mitchell <ian.mitchell@hey.com>
2022-11-06 16:31:26 -0800

    Add Docs

--- packages/discord-request/src/client.ts
+++ packages/discord-request/src/client.ts
@@ -3,20 +3,23 @@ import { Manager } from "./manager.js";
 import type { RequestData, RequestOptions } from "./types.js";
 import { RequestMethod } from "./types.js";
 
-/**
- *
- */
 export default class Client {
 	#manager: Manager;
 
+	/**
+	 * Creates a new Client for accessing the Discord API. This will
+	 * automatically queue requests and handle rate limits.
+	 * @param options - Configuration options for the Client.
+	 * @returns The Client instance.
+	 */
 	constructor(options: ManagerArgs = {}) {
 		this.#manager = new Manager(options);
 	}
 
 	/**
-	 *
-	 * @param token
-	 * @returns
+	 * Sets the Client's Bot Token.
+	 * @param token - The Discord Bot Token.
+	 * @returns The Client instance
 	 */
 	setToken(token: string) {
 		this.#manager.setToken(token);
@@ -24,49 +27,56 @@ export default class Client {
 	}
 
 	/**
-	 *
+	 * Get the Client's current User Agent.
 	 */
 	get userAgent() {
 		return this.#manager.config.userAgent;
 	}
 
 	/**
-	 *
+	 * Sets the Client's User Agent. This needs to include information about
+	 * the library and version being used to access the Discord API.
 	 */
 	set userAgent(value: string) {
 		this.#manager.config.userAgent = value;
 	}
 
 	/**
-	 *
+	 * Get the Client's current abort signal. If this signal triggers, all
+	 * pending requests will be aborted.
 	 */
 	get abortSignal() {
 		return this.#manager.shutdownSignal;
 	}
 
 	/**
-	 *
+	 * Sets the Client's abort signal. If this signal triggers, all pending
+	 * requests will be aborted.
 	 */
 	set abortSignal(signal: AbortSignal | null | undefined) {
 		this.#manager.shutdownSignal = signal;
 	}
 
 	/**
-	 *
+	 * Returns the Client's current global requests per second.
 	 */
 	get globalRequestsPerSecond() {
 		return this.#manager.config.globalRequestsPerSecond;
 	}
 
 	/**
-	 *
+	 * Sets the Client's global requests per second. You should not modify this
+	 * unless you are a very large bot and have been approved for a higher rate
+	 * limit by Discord. If you set this without Discord granting your bot a
+	 * higher rate limit, you risk your bot being banned.
 	 */
 	set globalRequestsPerSecond(value: number) {
 		this.#manager.config.globalRequestsPerSecond = value;
 	}
 
 	/**
-	 *
+	 * Get the Client's current Discord API configuration. This includes the API
+	 * URL, the API Version, and the CDN URL.
 	 */
 	get api() {
 		return {
@@ -77,7 +87,7 @@ export default class Client {
 	}
 
 	/**
-	 *
+	 * Sets the Client's Discord API configuration.
 	 */
 	set api({
 		api,
@@ -94,7 +104,7 @@ export default class Client {
 	}
 
 	/**
-	 *
+	 * Get the Client's current request configuration.
 	 */
 	get requestConfig() {
 		return {
@@ -105,7 +115,8 @@ export default class Client {
 	}
 
 	/**
-	 *
+	 * Sets the Client's current request configuration. This allows setting
+	 * global headers, request timeouts, and the retry count for a failed request.
 	 */
 	set requestConfig({
 		headers,
@@ -122,7 +133,9 @@ export default class Client {
 	}
 
 	/**
-	 *
+	 * Get the Client sweep intervals. These help prevent memory leaks by
+	 * periodically cleaning out unused, stored buckets and queues. In Edge and
+	 * Serverless runtimes, these are not needed and should not be configured.
 	 */
 	get sweepIntervals() {
 		return {
@@ -132,7 +145,9 @@ export default class Client {
 	}
 
 	/**
-	 *
+	 * Get the Client sweep intervals. These help prevent memory leaks by
+	 * periodically cleaning out unused, stored buckets and queues. In Edge and
+	 * Serverless runtimes, these are not needed and should not be configured.
 	 */
 	set sweepIntervals({
 		bucketSweepInterval,
@@ -146,7 +161,7 @@ export default class Client {
 	}
 
 	/**
-	 *
+	 * Get the current client callbacks.
 	 */
 	get callbacks() {
 		return {
@@ -158,7 +173,7 @@ export default class Client {
 	}
 
 	/**
-	 *
+	 * Set the client callbacks.
 	 */
 	set callbacks({
 		onBucketSweep,
@@ -178,10 +193,10 @@ export default class Client {
 	}
 
 	/**
-	 *
-	 * @param path
-	 * @param options
-	 * @returns
+	 * Sends an HTTP GET request to the Discord API.
+	 * @param path - The Discord API path to send a request to.
+	 * @param options - Request configuration options.
+	 * @returns The Discord API response.
 	 */
 	async get(path: string, options: RequestOptions = {}) {
 		return this.#request({
@@ -192,10 +207,10 @@ export default class Client {
 	}
 
 	/**
-	 *
-	 * @param path
-	 * @param options
-	 * @returns
+	 * Sends an HTTP POST request to the Discord API.
+	 * @param path - The Discord API path to send a request to.
+	 * @param options - Request configuration options.
+	 * @returns The Discord API response.
 	 */
 	async post(path: string, options: RequestOptions = {}) {
 		return this.#request({
@@ -206,10 +221,10 @@ export default class Client {
 	}
 
 	/**
-	 *
-	 * @param path
-	 * @param options
-	 * @returns
+	 * Sends an HTTP PUT request to the Discord API.
+	 * @param path - The Discord API path to send a request to.
+	 * @param options - Request configuration options.
+	 * @returns The Discord API response.
 	 */
 	async put(path: string, options: RequestOptions = {}) {
 		return this.#request({
@@ -220,10 +235,10 @@ export default class Client {
 	}
 
 	/**
-	 *
-	 * @param path
-	 * @param options
-	 * @returns
+	 * Sends an HTTP PATCH request to the Discord API.
+	 * @param path - The Discord API path to send a request to.
+	 * @param options - Request configuration options.
+	 * @returns The Discord API response.
 	 */
 	async patch(path: string, options: RequestOptions = {}) {
 		return this.#request({
@@ -234,10 +249,10 @@ export default class Client {
 	}
 
 	/**
-	 *
-	 * @param path
-	 * @param options
-	 * @returns
+	 * Sends an HTTP DELETE request to the Discord API.
+	 * @param path - The Discord API path to send a request to.
+	 * @param options - Request configuration options.
+	 * @returns The Discord API response.
 	 */
 	async delete(path: string, options: RequestOptions = {}) {
 		return this.#request({
@@ -247,11 +262,6 @@ export default class Client {
 		});
 	}
 
-	/**
-	 *
-	 * @param data
-	 * @returns
-	 */
 	async #request(data: RequestData) {
 		return this.#manager.queue(data);
 	}
