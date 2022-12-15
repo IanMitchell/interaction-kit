8eb5a54790de4fd9349d60730677d532bfd32e9f
Ian Mitchell <ian.mitchell@hey.com>
2022-12-15 11:18:42 -0800

    Add Permissions Support

--- packages/interaction-kit/src/commands/context-menu.ts
+++ packages/interaction-kit/src/commands/context-menu.ts
@@ -1,14 +1,16 @@
 import type {
 	APIApplicationCommand,
+	PermissionFlagsBits,
 	RESTPostAPIContextMenuApplicationCommandsJSONBody,
 } from "discord-api-types/v10";
 import type ContextMenuInteraction from "../interactions/application-commands/context-menu-interaction.js";
 import type { ContextMenuApplicationCommandType } from "../interactions/application-commands/context-menu-interaction.js";
-import type { InteractionKitCommand } from "../interfaces.js";
+import type { InteractionKitCommand, ValueOf } from "../interfaces.js";
 
 type ContextMenuArgs<T extends ContextMenuApplicationCommandType> = {
 	name: string;
 	type: T;
+	defaultPermissions?: Array<ValueOf<typeof PermissionFlagsBits>>;
 	handler: InteractionKitCommand<ContextMenuInteraction<T>>["handler"];
 };
 
@@ -17,13 +19,20 @@ export default class ContextMenu<T extends ContextMenuApplicationCommandType>
 {
 	name: string;
 	type: T;
+	defaultPermissions: null | bigint;
 	handler: InteractionKitCommand<ContextMenuInteraction<T>>["handler"];
 
-	constructor({ name, type, handler }: ContextMenuArgs<T>) {
+	constructor({ name, type, defaultPermissions, handler }: ContextMenuArgs<T>) {
 		// TODO: Validate: 1-32 lowercase character name matching ^[\w-]{1,32}$
 		this.name = name;
 		this.type = type;
 		this.handler = handler;
+
+		this.defaultPermissions =
+			defaultPermissions?.reduce(
+				(value, permission) => value | permission,
+				0n
+			) ?? null;
 	}
 
 	equals(schema: APIApplicationCommand): boolean {
@@ -39,6 +48,10 @@ export default class ContextMenu<T extends ContextMenuApplicationCommandType>
 			name: this.name,
 			type: this.type,
 		};
+
+		if (this.defaultPermissions != null) {
+			payload.default_member_permissions = this.defaultPermissions.toString();
+		}
 
 		return payload;
 	}
