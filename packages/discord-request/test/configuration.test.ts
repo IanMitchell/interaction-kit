102f3eeefb71eb0897a9fef4e8a542b98fcb1e1e
Ian Mitchell <ian.mitchell@hey.com>
2023-01-17 22:12:00 -0800

    Add audit log test

--- packages/discord-request/test/configuration.test.ts
+++ packages/discord-request/test/configuration.test.ts
@@ -1,4 +1,6 @@
-import { describe, test } from "vitest";
+import { describe, expect, test, vi } from "vitest";
+import Client from "../src/client.js";
+import { setMockResponse } from "./util/mock-fetch.js";
 
 describe("Sweeps", () => {
 	test.todo("Sweeps don't start with Interval 0");
@@ -17,5 +19,16 @@ describe("API URL", () => {
 describe("Headers", () => {
 	test.todo("Handles Authorization");
 
-	test.todo("Handles Audit Log");
+	test("Handles Audit Log", async () => {
+		const onRequest = vi.fn();
+		const client = new Client({ onRequest }).setToken("test");
+
+		setMockResponse({ status: 200, body: { success: true }, method: "POST" });
+		await client.post("/", { reason: "Mods, ban this user" });
+
+		const headers = new Headers(onRequest.mock.calls[0][2].headers);
+		expect(headers.get("X-Audit-Log-Reason")).toEqual(
+			"Mods%2C%20ban%20this%20user"
+		);
+	});
 });
