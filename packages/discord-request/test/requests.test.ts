28dd1338e624b7a1e59cafa8e89ae4a542971863
Ian Mitchell <ian.mitchell@hey.com>
2023-01-17 21:54:27 -0800

    Add JSON Test

--- packages/discord-request/test/requests.test.ts
+++ packages/discord-request/test/requests.test.ts
@@ -1,6 +1,6 @@
 import { describe, expect, test, vi } from "vitest";
-import Client from "../src/client";
-import { setMockResponse } from "./util/mock-fetch";
+import Client from "../src/client.js";
+import { setMockResponse } from "./util/mock-fetch.js";
 
 describe("Attachment Requests", () => {
 	test.todo("Handles Basic Attachments");
@@ -43,8 +43,21 @@ test("Fetches Data with correct HTTP Method", async () => {
 	setMockResponse({ status: 200, body: { success: true }, method: "DELETE" });
 	await client.delete("/");
 
+	// eslint-disable-next-line @typescript-eslint/no-unsafe-return
 	const methods = onRequest.mock.calls.map(([, , options]) => options.method);
 	expect(methods).toEqual(["GET", "POST", "PUT", "PATCH", "DELETE"]);
 });
 
-test.todo("Returns JSON Response");
+test("Returns JSON Response", async () => {
+	const client = new Client().setToken("test");
+	const body = { success: true, json: { key: "value " } };
+
+	setMockResponse({
+		status: 200,
+		body,
+		headers: { "Content-Type": "application/json" },
+	});
+	const response = await client.get("/");
+
+	expect(response).toEqual(body);
+});
