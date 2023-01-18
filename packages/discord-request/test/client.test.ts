6d8d18744a71fc920d9022a59cb6310ba244172f
Ian Mitchell <ian.mitchell@hey.com>
2023-01-17 20:07:44 -0800

    Finish Client Tests

--- packages/discord-request/test/client.test.ts
+++ packages/discord-request/test/client.test.ts
@@ -81,13 +81,78 @@ describe("Configuration", () => {
 		expect(client.globalRequestsPerSecond).toEqual(1);
 	});
 
-	test.todo("api");
+	test("api", () => {
+		const client = new Client();
+		client.api = {
+			api: "apiOverride",
+			version: 1,
+			cdn: "cdnOverride",
+		};
+		expect(client.api).toEqual({
+			api: "apiOverride",
+			version: 1,
+			cdn: "cdnOverride",
+		});
+	});
 
-	test.todo("requestConfig");
+	test("requestConfig", () => {
+		const client = new Client();
+		client.requestConfig = {
+			headers: { test: "vitest" },
+			retries: 1,
+			timeout: 1,
+		};
+		expect(client.requestConfig).toEqual({
+			headers: { test: "vitest" },
+			retries: 1,
+			timeout: 1,
+		});
+	});
 
-	test.todo("sweepIntervals");
+	test("sweepIntervals", () => {
+		const client = new Client();
+		client.sweepIntervals = {
+			bucketSweepInterval: 31,
+			queueSweepInterval: 31,
+		};
+		expect(client.sweepIntervals).toEqual({
+			bucketSweepInterval: 31,
+			queueSweepInterval: 31,
+		});
+	});
 
-	test.todo("callbacks");
+	test("callbacks", () => {
+		const onBucketSweep = () => {
+			console.log("bucket sweep");
+		};
+
+		const onQueueSweep = () => {
+			console.log("queue sweep");
+		};
+
+		const onRateLimit = () => {
+			console.log("rate limit");
+		};
+
+		const onRequest = () => {
+			console.log("request");
+		};
+
+		const client = new Client();
+		client.callbacks = {
+			onBucketSweep,
+			onQueueSweep,
+			onRateLimit,
+			onRequest,
+		};
+
+		expect(client.callbacks).toEqual({
+			onBucketSweep,
+			onQueueSweep,
+			onRateLimit,
+			onRequest,
+		});
+	});
 });
 
 describe("HTTP Methods", () => {
