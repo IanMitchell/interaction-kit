9e834561f68c3c813d6c5ded7047c992c62320ef
Ian Mitchell <ian.mitchell@hey.com>
2023-02-07 22:10:05 -0800

    Switch to Next
    I ran the numbers, I'm cheap

---
+++ apps/interactionkit.dev/app/page.tsx
@@ -0,0 +1,5 @@
+import { H1 } from "@/components/h1";
+
+export default function Page() {
+	return <H1>Hello World</H1>;
+}
---
+++ apps/interactionkit.dev/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	darkMode: "class",
+	content: [
+		"./app/**/*.{js,ts,jsx,tsx}",
+		"./page/**/*.{js,ts,jsx,tsx}",
+		"./components/**/*.{js,ts,jsx,tsx}",
+	],
+	theme: {},
+};
